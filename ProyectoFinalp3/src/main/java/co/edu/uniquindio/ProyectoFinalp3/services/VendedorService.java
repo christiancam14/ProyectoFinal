a33a095f9a6685c64eef55a9ed606a470454f1a9
Maryam <maryamtato1509@gmail.com>
2024-10-10 22:07:26 -0500

    .

--- ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/services/VendedorService.java
+++ ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/services/VendedorService.java
@@ -2,7 +2,7 @@ package co.edu.uniquindio.ProyectoFinalp3.services;
 
 import co.edu.uniquindio.ProyectoFinalp3.models.Producto;
 import co.edu.uniquindio.ProyectoFinalp3.models.Vendedor;
-import co.edu.uniquindio.ProyectoFinalp3.Repository.VendedorRepository;
+import co.edu.uniquindio.ProyectoFinalp3.repository.VendedorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
