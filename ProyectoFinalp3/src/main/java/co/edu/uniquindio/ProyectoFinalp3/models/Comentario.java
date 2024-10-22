4f9284a98da1dfd749a8b503eb87a7dc7f438383
nicolas <jnch2005@gmail.com>
2024-10-21 22:46:02 -0500

    cambio de formato a sql

--- ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/models/Comentario.java
+++ ProyectoFinalp3/src/main/java/co/edu/uniquindio/ProyectoFinalp3/models/Comentario.java
@@ -1,15 +1,26 @@
 package co.edu.uniquindio.ProyectoFinalp3.models;
 
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import java.io.Serializable;
 
-class Comentario {
+class Comentario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
     private LocalDateTime fechaYhora;
+
+    @Column(nullable = false, length = 1000)
     private String contenido;
 
     public Comentario(String contenido) {
         this.fechaYhora = LocalDateTime.now();
         this.contenido = contenido;
     }
+
     public LocalDateTime getFechaYhora() {
         return fechaYhora;
     }
